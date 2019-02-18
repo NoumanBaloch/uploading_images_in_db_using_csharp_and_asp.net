5d33dd6ea766fca3c614d29653b50a203905c309
Nouman Baloch <mrnoumanbaloch@gmail.com>
2019-02-18 21:40:56 +0500

    Image Uploading over DB using ASP.Net and C#

---
+++ Default.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+       
+    table
+    {
+        border-collapse:collapse;        
+    }
+    </style>
+</head>
+<body>
+<h1 align="center">Welcome to the system</h1>
+<form id="form1" runat="server">
+    <div >
+<table align="center" >
+<tr>
+<td><p>Username:</p> </td>
+<td><asp:TextBox ID="TextBox1" runat="server"></asp:TextBox></td>
+</tr>
+<tr>
+<td><p>Upload Image:</p> </td>
+<td><asp:FileUpload ID="FileUpload1" runat="server" /></td>
+</tr>
+<tr>
+<td colspan="2" align="center"> <asp:Button ID="Button1" runat="server" onclick="Button1_Click" Text="Submit" /></td>
+
+</tr>
+<tr><td colspan="2" align="center"><asp:Label ID="Label1" runat="server"></asp:Label></td></tr>
+
+</table>
+  </div>
+    </form>  
+    
+    
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=.\SQLEXPRESS;AttachDbFilename=F:\Graphical_Authentication_Based_on_CCP\App_Data\Database.mdf;Integrated Security=True;User Instance=True");
+       
+        if (FileUpload1.HasFile)
+        {
+            string strname = FileUpload1.FileName.ToString();
+            FileUpload1.PostedFile.SaveAs(Server.MapPath("~/upload/") + strname);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("insert into upload values('" + TextBox1.Text + "','" + strname + "')", con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            Label1.Visible = true;
+            Label1.Text = "Image Uploaded successfully";
+            TextBox1.Text = "";
+        }
+        else
+        {
+            Label1.Visible = true;
+            Label1.Text = "Plz upload the image!!!!";
+        }  
+
+    }
+}
